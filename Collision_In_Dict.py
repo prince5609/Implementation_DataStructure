f1c7426d4a4ec07fe39a689ca179296724e4a618
prince5609 <prince.tyagi98@gmail.com>
2021-05-03 21:05:45 +0530

    Working on Data_Structures

---
+++ Collision_In_Dict.py
@@ -0,0 +1,45 @@
+class Dict_Table:
+    def __init__(self):
+        self.max = 100
+        self.arr = [[] for i in range(self.max)]
+
+    def get_Dict(self,key):
+        d = 0
+        for char in key:
+            d += ord(char)
+        return d % self.max
+
+    def __getitem__(self, key):
+        d = self.get_Dict(key)
+        for element in self.arr[d]:
+            if element[0] == key:
+                return print(element[1])
+
+
+    def __setitem__(self, key, value):
+        d = self.get_Dict(key)
+        element_found = False
+        for ind,element in enumerate(self.arr[d]):
+            if len(element) == 2 and element[0] == key:
+                self.arr[d][ind] = (key,value)
+                element_found = True
+                break
+        if not element_found:
+            self.arr[d].append((key,value))
+
+    def __delitem__(self, key):
+        d = self.get_Dict(key)
+        for ind,element in enumerate(self.arr[d]):
+            if element[0] == key:
+                del self.arr[d][ind]
+
+t = Dict_Table()
+t.__setitem__("march 6",1)
+t.__setitem__("march 17",12)
+t.__getitem__("march 17")
+t.__getitem__("march 6")
+t.__delitem__("march 17")
+t.__getitem__("march 17")
+
+
+
---
+++ Dict_Table.py
@@ -0,0 +1,30 @@
+class Dict_Table:
+    def __init__(self):
+        self.max = 100
+        self.arr = [None for i in range(self.max)]
+
+    def get_dict(self,key):
+        d = 0
+        for char in key:
+            d += ord(char)
+        return d % self.max
+
+    def __setitem__(self, key,val):              # we can use add here in place of __setitem__(will be called by it's name)
+        d = self.get_dict(key)
+        self.arr[d] = val
+
+    def __getitem__(self, key):                   # only get can be use in place of __getitem__
+        d = self.get_dict(key)
+        return print(self.arr[d])
+
+    def __delitem__(self, key):
+        d = self.get_dict(key)
+        self.arr[d] = None
+
+t = Dict_Table()
+t.__setitem__("prince",18)
+t.__setitem__("tyagi",20)
+t.__getitem__("prince")
+t.__getitem__("tyagi")
+t.__delitem__("prince")
+t.__getitem__("prince")
