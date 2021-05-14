43bc6c98eada0448e8cdc5e5038a40850d678fdd
prince5609 <prince.tyagi98@gmail.com>
2021-05-14 06:53:14 +0530

    Working on Data_Structures Merge_Sort

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,32 +1,34 @@
 def merge_sort(array):
     if len(array) <= 1:
-        return array
+        return
     mid = len(array)//2
     left = array[:mid]
     right = array[mid:]
-    left = merge_sort(left)
-    right = merge_sort(right)
-    return merge_two_sorted_list(left, right)
+    merge_sort(left)
+    merge_sort(right)
+    merge_two_sorted_list(left, right, array)
 
-def merge_two_sorted_list(a, b):
-    sorted_arr = []
-    i = j = 0
+def merge_two_sorted_list(a, b, array):
+    i = j = k = 0
     while i < len(a) and j < len(b):
         if a[i] > b[j]:
-            sorted_arr.append(b[j])
+            array[k] = b[j]
             j += 1
         elif a[i] < b[j]:
-            sorted_arr.append(a[i])
+            array[k] = a[i]
             i += 1
+        k += 1
     while i < len(a):
-        sorted_arr.append(a[i])
+        array[k] = a[i]
         i += 1
+        k += 1
     while j < len(b):
-        sorted_arr.append(b[j])
+        array[k] = b[j]
         j += 1
-    return sorted_arr
-
-print(merge_sort([18,14,7,25,3,29,20,30,12]))
+        k += 1
+array = [18,14,7,25,3,29,20,30,12]
+merge_sort(array)
+print(array)
 
 
 
