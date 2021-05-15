68b7130014a6d0874833c5a6272ee6066d96e2b2
prince5609 <prince.tyagi98@gmail.com>
2021-05-15 17:46:11 +0530

    Working on Data_Structures Selection_Sort

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,13 +1,15 @@
 def selection_sort(array):
-    i = 0
-    while i <= len(array)-2:
-        m = array.index(min(array[i+1:len(array)]))
-        if array[i] > array[m]:
-            temp = array[i]
-            array[i] = array[m]
-            array[m] = temp
-        i += 1
+    for i in range(len(array)-1):
+        min_index = i
+        for j in range(min_index+1,len(array)):
+            if array[j] < array[min_index]:
+                min_index = j
+        if i != min_index:
+            array[i],array[min_index] = array[min_index],array[i]
     return array
+
 print(selection_sort([21,38,29,17,4,25,11,32,9]))
 print(selection_sort([1,0]))
 print(selection_sort([1,2,3,4,5,6,7,8,9]))
+print(selection_sort([55,33,9,41,6,1,7,2,5,879,5,5,4,85,5646,2585,5564]))
+print(selection_sort([5,5,5,5,5,5,5,5,5]))
