276949769126dbc1e00077ed0ed2c893e64a9f40
prince5609 <prince.tyagi98@gmail.com>
2021-05-16 15:42:12 +0530

    Working on Data_Structures Count_Sort

---
+++ Count_Sort.py
@@ -0,0 +1,18 @@
+def count_sort(array):
+    maximum = max(array)
+    count_array = [0] * (maximum+1)
+    while maximum >= min(array):
+        count = array.count(maximum)
+        count_array[maximum] = count
+        maximum -= 1
+    for i in range(len(count_array)-1):
+        count_array[i+1] = count_array[i+1] + count_array[i]
+    result_array = [0]*len(array)
+    for j in range(len(array)):
+        value = count_array[array[j]]
+        result_array[value-1] = array[j]
+        count_array[array[j]] = count_array[array[j]] -1
+    return result_array
+print(count_sort([2,1,2,6,0,4,3,3]))
+print(count_sort([2,4,9,6,7,9,3,4,8,1,5]))
+
