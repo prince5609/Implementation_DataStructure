51009d2f9db891d7a58231d3321d71bdf7479475
prince5609 <prince.tyagi98@gmail.com>
2021-05-19 15:44:48 +0530

    Working on Data_Structures Selection_Sort

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,15 +1,17 @@
 def selection_sort(array):
-    for i in range(len(array)-1):
+    for i in range(len(array) - 1):
         minimum = min(array[i + 1:len(array)])
         if array[i] > minimum:
-            index = array[i+1:len(array)].index(minimum)
-            array[i],array[index+i+1] = array[index+i+1],array[i]
+            index = array[i + 1:len(array)].index(minimum)
+            array[i], array[index + i + 1] = array[index + i + 1], array[i]
         elif array[i] == minimum:
             index = array[i + 1:len(array)].index(minimum)
-            array[i+1],array[index] = array[index],array[i+1]
+            array[i + 1], array[index] = array[index], array[i + 1]
     return array
-print(selection_sort([21,38,29,17,4,25,11,32,9]))
-print(selection_sort([1,0]))
-print(selection_sort([1,2,3,4,5,6,7,8,9]))
-print(selection_sort([55,33,9,41,6,1,7,2,5,879,5,5,4,85,5646,2585,5564]))
-print(selection_sort([5,5,5,5,5,5,5,5,5]))
+
+
+print(selection_sort([21, 38, 29, 17, 4, 25, 11, 32, 9]))
+print(selection_sort([1, 0]))
+print(selection_sort([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+print(selection_sort([55, 33, 9, 41, 6, 1, 7, 2, 5, 879, 5, 5, 4, 85, 5646, 2585, 5564]))
+print(selection_sort([5, 5, 5, 5, 5, 5, 5, 5, 5]))
