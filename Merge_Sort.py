be86e2787721a8e87a037bb6e71e0aaa3caa5f14
prince5609 <prince.tyagi98@gmail.com>
2021-05-21 23:36:54 +0530

    Working on Data_Structures Merge_Sort

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,35 +1,34 @@
-def merge_sort(array):
-    if len(array) <= 1:
+def merge_sort(data_list):
+    if len(data_list) <= 1:
         return
-    mid = len(array)//2
-    left = array[:mid]
-    right = array[mid:]
+    mid = len(data_list) // 2
+    left = data_list[:mid]
+    right = data_list[mid:]
     merge_sort(left)
     merge_sort(right)
-    merge_two_sorted_list(left, right, array)
+    merge_two_sorted_list(left, right, data_list)
+    return data_list
 
-def merge_two_sorted_list(a, b, array):
+
+def merge_two_sorted_list(a, b, data_list):
     i = j = k = 0
     while i < len(a) and j < len(b):
         if a[i] > b[j]:
-            array[k] = b[j]
+            data_list[k] = b[j]
             j += 1
         elif a[i] < b[j]:
-            array[k] = a[i]
+            data_list[k] = a[i]
             i += 1
         k += 1
     while i < len(a):
-        array[k] = a[i]
+        data_list[k] = a[i]
         i += 1
         k += 1
     while j < len(b):
-        array[k] = b[j]
+        data_list[k] = b[j]
         j += 1
         k += 1
-array = [18,14,7,25,3,29,20,30,12]
-merge_sort(array)
-print(array)
 
 
-
-
+array = [18, 14, 7, 25, 3, 29, 20, 30, 12]
+print(merge_sort(array))
