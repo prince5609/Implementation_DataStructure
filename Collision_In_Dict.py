d405b085b1a78366e51c6aafe7cc02e7ee5dde14
prince5609 <prince.tyagi98@gmail.com>
2021-05-25 17:06:19 +0530

    Working on Data_Structures Dict_Table

--- Collision_In_Dict.py
+++ Collision_In_Dict.py
@@ -1,9 +1,9 @@
 class Dict_Table:
     def __init__(self):
-        self.max = 100
+        self.max = 10
         self.arr = [[] for i in range(self.max)]
 
-    def get_Dict(self,key):
+    def get_Dict(self, key):
         d = 0
         for char in key:
             d += ord(char)
@@ -15,13 +15,12 @@ class Dict_Table:
             if element[0] == key:
                 return print(element[1])
 
-
     def __setitem__(self, key, value):
         d = self.get_Dict(key)
         element_found = False
-        for ind,element in enumerate(self.arr[d]):
+        for ind, element in enumerate(self.arr[d]):
             if len(element) == 2 and element[0] == key:
-                self.arr[d][ind] = (key,value)
+                self.arr[d][ind] = (key, value)
                 element_found = True
                 break
         if not element_found:
@@ -29,17 +28,21 @@ class Dict_Table:
 
     def __delitem__(self, key):
         d = self.get_Dict(key)
-        for ind,element in enumerate(self.arr[d]):
+        for ind, element in enumerate(self.arr[d]):
             if element[0] == key:
                 del self.arr[d][ind]
 
+    def print(self):
+        return print(self.arr)
+
+
 t = Dict_Table()
-t.__setitem__("march 6",1)
-t.__setitem__("march 17",12)
+t.__setitem__("march 6", 1)
+t.print()
+t.__setitem__("march 17", 12)
+t.print()
 t.__getitem__("march 17")
 t.__getitem__("march 6")
 t.__delitem__("march 17")
+t.print()
 t.__getitem__("march 17")
-
-
-
