1cb096f62af8904464dff9ffed3b033c39b407fa
prince5609 <prince.tyagi98@gmail.com>
2021-05-25 17:09:08 +0530

    Working on Data_Structures Stack

--- Stack.py
+++ Stack.py
@@ -1,4 +1,6 @@
 from collections import deque
+
+
 class stack:
     def __init__(self):
         self.a = deque()
@@ -6,8 +8,8 @@ class stack:
     def pop(self):
         return self.a.pop()
 
-    def push(self,val):
-        return (self.a.append(val))
+    def push(self, val):
+        return self.a.append(val)
 
     def peek(self):
         return self.a[-1]
@@ -18,6 +20,7 @@ class stack:
     def size(self):
         return len(self.a)
 
+
 obj = stack()
 obj.push(3)
 obj.push(4)
@@ -25,5 +28,3 @@ obj.push(5)
 obj.push(8)
 obj.pop()
 print(obj.a)
-
-
