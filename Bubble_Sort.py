feb5e8e23ec0492ebe8abef8398ab64924696995
prince5609 <prince.tyagi98@gmail.com>
2021-05-25 16:53:20 +0530

    Working on Data_Structures Bubble_Sort

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,15 +1,16 @@
-def Bubble_Sort(array):
-
-    n = len(array) -1
+def bubble_sort(array):
+    n = len(array) - 1
     for j in range(n):
         swapped = False
-        for i in range(n-j):
-            if array[i] > array[i+1]:
+        for i in range(n - j):
+            if array[i] > array[i + 1]:
                 temp = array[i]
-                array[i] = array[i+1]
-                array[i+1] = temp
+                array[i] = array[i + 1]
+                array[i + 1] = temp
                 swapped = True
         if not swapped:
             break
     return array
-print(Bubble_Sort([8,4,6,1,2,3,7,9,5]))
+
+
+print(bubble_sort([8, 4, 6, 1, 2, 3, 7, 9, 5]))
