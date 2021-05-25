390f9b6d5be86a250d9c36968454c533594e6f2c
prince5609 <prince.tyagi98@gmail.com>
2021-05-25 17:02:59 +0530

    Working on Data_Structures Dict_Table

--- Dict_Table.py
+++ Dict_Table.py
@@ -1,19 +1,19 @@
 class Dict_Table:
     def __init__(self):
-        self.max = 100
+        self.max = 10
         self.arr = [None for i in range(self.max)]
 
-    def get_dict(self,key):
+    def get_dict(self, key):
         d = 0
         for char in key:
             d += ord(char)
         return d % self.max
 
-    def __setitem__(self, key,val):
+    def __setitem__(self, key, val):
         d = self.get_dict(key)
         self.arr[d] = val
 
-    def __getitem__(self, key):                   # only get can be use in place of __getitem__
+    def __getitem__(self, key):  # only get can be use in place of __getitem__
         d = self.get_dict(key)
         return print(self.arr[d])
 
@@ -21,10 +21,18 @@ class Dict_Table:
         d = self.get_dict(key)
         self.arr[d] = None
 
+    def print(self):
+        return print(self.arr)
+
+
 t = Dict_Table()
-t.__setitem__("prince",18)
-t.__setitem__("tyagi",20)
-t.__getitem__("prince")
-t.__getitem__("tyagi")
-t.__delitem__("prince")
-t.__getitem__("prince")
+t.__setitem__("Name", "Prince")
+t.print()
+t.__setitem__("Age", 25)
+t.print()
+t.__setitem__("Gender", "Male")
+t.print()
+t.__getitem__("Name")
+t.__getitem__("Age")
+t.__delitem__("Gender")
+t.print()
