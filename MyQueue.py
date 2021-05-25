c37c8be1dc3cda4678985ab162d513b8753c18f3
prince5609 <prince.tyagi98@gmail.com>
2021-05-25 17:07:52 +0530

    Working on Data_Structures Queue

--- MyQueue.py
+++ MyQueue.py
@@ -1,4 +1,6 @@
 from collections import deque
+
+
 class MyQueue:
     def __init__(self):
         self.a = deque()
@@ -6,14 +8,15 @@ class MyQueue:
     def pop(self):
         return self.a.pop()
 
-    def push(self,val):
-        return (self.a.appendleft(val))
+    def push(self, val):
+        return self.a.appendleft(val)
 
     def is_empty(self):
         return len(self.a) == 0
 
     def size(self):
         return len(self.a)
+
 
 obj = MyQueue()
 obj.push(3)
@@ -22,5 +25,3 @@ obj.push(5)
 obj.push(8)
 obj.pop()
 print(obj.a)
-
-
