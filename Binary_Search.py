d718499281132b8e5a31f680418131d0cce3234d
prince5609 <prince.tyagi98@gmail.com>
2021-05-26 16:52:51 +0530

    Working on Data_Structures Binary_Search

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,21 +1,22 @@
-def binary_search(array,x):
+def binary_search(array, target):
     start = 0
     end = len(array) - 1
-    mid = (len(array) - start)//2
+    mid = (len(array) - start) // 2
     while end > start:
-        if x == array[start]:
+        if target == array[start]:
             return start
-        elif x == array[end]:
+        elif target == array[end]:
             return end
-        elif x == array[mid]:
-             return mid
-        elif x > array[mid]:
+        elif target == array[mid]:
+            return mid
+        elif target > array[mid]:
             start = mid + 1
-            mid = ((end - start)//2) + start
-        elif x < array[mid]:
+            mid = ((end - start) // 2) + start
+        elif target < array[mid]:
             end = mid - 1
-            mid = ((end - start)//2) + start
-print(binary_search([1,2,3,4,5,6,7,8,9],7))
-print(binary_search([],0))
-print(binary_search([10,20,30,40,50,100],10))
+            mid = ((end - start) // 2) + start
 
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 7))
+print(binary_search([], 0))
+print(binary_search([10, 20, 30, 40, 50, 100], 10))
