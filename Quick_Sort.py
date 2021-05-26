da037ccadcdb8c04d1f0a4732a85332e5f7a1316
prince5609 <prince.tyagi98@gmail.com>
2021-05-26 15:17:16 +0530

    Working on Data_Structures Quick_Sort

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -1,35 +1,33 @@
-array = [11, 9, 2, 7, 29, 15, 28]
-class Quick_Sort:
-    def swap(self, a, b, array):
-        if a!=b:
-            temp = array[a]
-            array[a] = array[b]
-            array[b] = temp
+def swap(a, b, array):
+    if a != b:
+        temp = array[a]
+        array[a] = array[b]
+        array[b] = temp
 
 
-    def partition(self,array,start,end):
-        pivot_index = start
-        pivot = array[pivot_index]
-
-        while start < end:
-            while start < len(array) and array[start] <= pivot:
-                start += 1
-            while pivot < array[end]:
-                end -= 1
-            if start < end:
-                self.swap(start, end, array)
-        self.swap(pivot_index, end, array)
-        return end
-
-
-    def quick_sort(self, array, start, end):
+def partition(array, start, end):
+    pivot_index = start
+    pivot = array[pivot_index]
+    while start < end:
+        while start < len(array) and array[start] <= pivot:
+            start += 1
+        while pivot < array[end]:
+            end -= 1
         if start < end:
-            pi = self.partition(array, start, end)
-            self.quick_sort(array, start, pi-1)
-            self.quick_sort(array, pi+1, end)
-        return array
+            swap(start, end, array)
+    swap(pivot_index, end, array)
+    return end
 
 
-obj = Quick_Sort()
-print(obj.quick_sort(array, 0, len(array)-1))
+def quick_sort(array, start, end):
+    if start < end:
+        pi = partition(array, start, end)
+        quick_sort(array, start, pi - 1)
+        quick_sort(array, pi + 1, end)
+    return array
 
+
+data = [11, 9, 2, 7, 29, 15, 28]
+data2 = [55, 33, 9, 41, 6, 1, 7, 2, 5, 879, 5, 5, 4, 85, 5646, 2585, 5564]
+print(quick_sort(data, 0, len(data) - 1))
+print(quick_sort(data2, 0, len(data2) - 1))
