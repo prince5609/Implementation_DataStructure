ada7e2ff488e8b54f121afa6c50cde5315048f2f
prince5609 <prince.tyagi98@gmail.com>
2021-05-30 15:36:33 +0530

    Working on Data_Structures Insertion_Sort

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,18 +1,13 @@
-def swap(a, b, array):
-    if a != b:
-        temp = array[a]
-        array[a] = array[b]
-        array[b] = temp
-
-
 def insertion_sort(array):
     for i in range(1, len(array)):
-        for j in range(0, i - 1):
-            swapped = False
-            if array[i] < array[j]:
-                swap(i, j, array)
-                swapped = True
+        j = i - 1
+        anchor = array[i]
+        while j >= 0 and anchor < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+        array[j + 1] = anchor
     return array
 
 
+print(insertion_sort([8, 4, 6, 1, 2, 3, 7, 6, 9, 5]))
 print(insertion_sort([21, 29, 38, 17, 4, 25, 11, 32, 9]))
