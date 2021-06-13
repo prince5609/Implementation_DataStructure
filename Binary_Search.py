bb7cdd1fdb97a94d3f22d165cb9552a5bc697415
prince5609 <80540000+prince5609@users.noreply.github.com>
2021-06-13 22:53:19 +0530

    Binary_Search

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,22 +1,19 @@
 def binary_search(array, target):
+    if target not in array:
+        return None
     start = 0
     end = len(array) - 1
-    mid = (end - start) // 2
-    while end > start:
-        if target == array[start]:
-            return start
-        elif target == array[end]:
-            return end
-        elif target == array[mid]:
-            return mid
-        elif target > array[mid]:
+    mid = (end + start) >> 1
+    while array[mid] != target:
+        if target > array[mid]:
             start = mid + 1
-            mid = ((end - start) // 2) + start
-        elif target < array[mid]:
+        else:
             end = mid - 1
-            mid = ((end - start) // 2) + start
+        mid = (end + start) >> 1
+    return mid
 
 
 print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 7))
-print(binary_search([1, 2, 3], 0))
 print(binary_search([10, 20, 30, 40, 50, 100], 10))
+print(binary_search([5, 8, 12, 26, 38, 49, 65, 98, 100], 38))
+print(binary_search([5, 8, 12, 26, 38, 49, 65, 98, 100], 1000))
